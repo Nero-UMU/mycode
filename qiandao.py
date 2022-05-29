c3b26537b0aa5f5b10b6d1ed73b4e59eb9222843
nero <2572591652@qq.com>
2022-05-29 21:47:30 +0800

    backup

---
+++ qiandao.py
@@ -0,0 +1,10 @@
+from traceback import print_tb
+
+
+yuan = "1732 2514 1344 0356 0451 6671 0055"
+mi = "9678 6668 4219 6847 5395 6403 9175"
+
+arr1 = yuan.split(" ")
+arr2 = mi.split(" ")
+print(arr1)
+print(arr2)
